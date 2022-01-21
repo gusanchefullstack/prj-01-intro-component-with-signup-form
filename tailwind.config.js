06a8aed820ba2fb4e94e960b2fd2efed97896bb3
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-20 19:00:21 -0500

    💄  Add tailwind classes for mobile rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myRed: "hsl(0, 100%, 74%)",
+        myGreen: "hsl(154, 59%, 51%)",
+        myBlue: "hsl(248, 32%, 49%)",
+        myDarkBlue: "hsl(249, 10%, 26%)",
+        myGrayishBlue: "hsl(246, 25%, 77%)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        mobile: "url(images/bg-intro-mobile.png)",
+        desktop: "url(images/bg-intro-desktop.png)",
+      },
+    },
   },
   plugins: [],
 };
